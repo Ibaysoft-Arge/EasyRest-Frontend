b45ba504979f648034aecb4ed3a276c00884bf55
ibrahim AYDOĞDU <ibaysoft@gmail.com>
2024-10-14 17:28:46 +0300

    aaa
    ddd

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // app.js
 
-var app = angular.module('myApp', ['ngRoute','pascalprecht.translate','ngSanitize']);
+var app = angular.module('myApp', ['ngRoute', 'pascalprecht.translate', 'ngSanitize']);
 
 // Sayfalama için özel filtre
-app.filter('startFrom', function() {
-  return function(input, start) {
+app.filter('startFrom', function () {
+  return function (input, start) {
     if (!input || !input.length) { return; }
     start = +start; // string'i int'e çevir
     return input.slice(start);
@@ -12,133 +12,127 @@ app.filter('startFrom', function() {
 });
 
 // Routing ayarları
-app.config(function($routeProvider, $httpProvider) {
+app.config(function ($routeProvider, $httpProvider) {
   // Interceptor ekle
   $httpProvider.interceptors.push('AuthInterceptor');
 
   $routeProvider
-    .when('/login', {
-      templateUrl: 'views/login.html',
-      controller: 'LoginController', // Büyük harfli 'L' ve 'C' kullanın
-      isLoginPage: true,
-      layout:'/layout/main-layout.html',
-    })
-    .when('/register', {
-      templateUrl: 'views/register.html',
-      controller: 'registerController', 
-      isLoginPage: false,
-      layout:'/layout/main-layout.html',
-    })
-    .when('/forgotpassword', {
-      templateUrl: 'views/forgotPassword.html',
-      controller: 'LoginController', 
-      isLoginPage: false,
-      layout:'/layout/main-layout.html',
-    })
-    .when('/resetpassword', {
-      templateUrl: 'views/resetPassword.html',
-      controller: 'LoginController', 
-      isLoginPage: false,
-      layout:'/layout/main-layout.html',
-    })
-    .when('/dashboard', {
-      templateUrl: 'views/dashboard.html',
-      controller: 'dashboardController', 
-      isLoginPage: false,
-      layout:'/layout/dashboard-layout.html',
-      // Bu rotaya erişmek için giriş yapılmış olmalı
-      resolve: {
-        auth: function(authService, $location) {
-          if (!authService.isLoggedIn()) {
-            $location.path('/login');
-          }
+  .when('/login', {
+    templateUrl: 'views/login.html',
+    controller: 'LoginController', // Büyük harfli 'L' ve 'C' kullanın
+    isLoginPage: true,
+    layout:'/layout/main-layout.html',
+  })
+  .when('/register', {
+    templateUrl: 'views/register.html',
+    controller: 'registerController', 
+    isLoginPage: false,
+    layout:'/layout/main-layout.html',
+  })
+  .when('/forgotpassword', {
+    templateUrl: 'views/forgotPassword.html',
+    controller: 'LoginController', 
+    isLoginPage: false,
+    layout:'/layout/main-layout.html',
+  })
+  .when('/resetpassword', {
+    templateUrl: 'views/resetPassword.html',
+    controller: 'LoginController', 
+    isLoginPage: false,
+    layout:'/layout/main-layout.html',
+  })
+  .when('/dashboard', {
+    templateUrl: 'views/dashboard.html',
+    controller: 'dashboardController', 
+    isLoginPage: false,
+    layout:'/layout/dashboard-layout.html',
+    // Bu rotaya erişmek için giriş yapılmış olmalı
+    resolve: {
+      auth: function(authService, $location) {
+        if (!authService.isLoggedIn()) {
+          $location.path('/login');
         }
       }
-    })
-    .when('/sayfa1', {
-      templateUrl: 'views/sayfa1.html',
-      controller: 'dashboardController', 
-      isLoginPage: false,
-      layout:'/layout/dashboard-layout.html',
-      // Bu rotaya erişmek için giriş yapılmış olmalı
-      resolve: {
-        auth: function(authService, $location) {
-          if (!authService.isLoggedIn()) {
-            $location.path('/login');
-          }
+    }
+  })
+  .when('/sayfa1', {
+    templateUrl: 'views/sayfa1.html',
+    controller: 'dashboardController', 
+    isLoginPage: false,
+    layout:'/layout/dashboard-layout.html',
+    // Bu rotaya erişmek için giriş yapılmış olmalı
+    resolve: {
+      auth: function(authService, $location) {
+        if (!authService.isLoggedIn()) {
+          $location.path('/login');
         }
       }
-    })
-    .when('/landing', {
-      templateUrl: 'views/landing.html',
-      controller: 'paketController', // Büyük harfli 'D' ve 'C' kullanın
-      isLoginPage: true,
-      // Bu rotaya erişmek için giriş yapılmış olmalı
-    })
-    .otherwise({
-      redirectTo: '/landing'
-    });
+    }
+  })
+  .when('/landing', {
+    templateUrl: 'views/landing.html',
+    controller: 'paketController', // Büyük harfli 'D' ve 'C' kullanın
+    isLoginPage: true,
+  })
+  .otherwise({
+    redirectTo: '/landing'
+  });
 });
 
 // Çeviri ayarları
-app.config(function($translateProvider) {
+app.config(function ($translateProvider) {
   $translateProvider.useStaticFilesLoader({
     prefix: '/languages/',
     suffix: '.json'
   });
 
-  var userLang = navigator.language || navigator.userLanguage; // Tarayıcı dilini al
-  console.log("Tarayıcı Dili: ", userLang);
-
-  // Eğer tarayıcı dili Türkçe ise 'tr', değilse 'en' olarak varsayılan dili ayarla
+  var userLang = navigator.language || navigator.userLanguage;
   if (userLang.startsWith('tr')) {
     $translateProvider.preferredLanguage('tr');
   } else {
     $translateProvider.preferredLanguage('en');
   }
 
-  // Varsayılan fallback dilini de ayarla
   $translateProvider.fallbackLanguage('en');
 });
 
-// Auth Interceptor (Dairesel Bağımlılığı Kırmak İçin $injector Kullanıldı)
-app.factory('AuthInterceptor', function($q, $injector) {
+// Auth Interceptor
+app.factory('AuthInterceptor', function ($q, $injector) {
   return {
-    request: function(config) {
+    request: function (config) {
       var authService = $injector.get('authService');
       var token = authService.getToken();
       if (token) {
-        config.headers.Authorization = 'Bearer ' + token; // 'Bearer ' ekleyin
-        console.log('AuthInterceptor: Token eklendi.');
-      } else {
-        console.log('AuthInterceptor: Token bulunamadı.');
+        config.headers.Authorization = 'Bearer ' + token;
       }
       return config;
     },
-    responseError: function(response) {
+    responseError: function (response) {
       if (response.status === 401 || response.status === 403) {
         var authService = $injector.get('authService');
         authService.logOut();
         window.location = '#!/login';
-        console.log('AuthInterceptor: Unauthorized veya Forbidden hatası.');
       }
       return $q.reject(response);
     }
   };
 });
 
-app.run(function($rootScope, $location, authService, $translate, $templateRequest, $compile) {
+app.run(function ($rootScope, $location, authService, $translate, $templateRequest, $compile) {
   // Route değişimlerinde layout yükleme ve auth kontrolü
-  $rootScope.$on('$routeChangeStart', function(event, next, current) {
+  $rootScope.$on('$routeChangeStart', function (event, next, current) {
     // Eğer bir layout belirlenmişse, onu yükle
     if (next.$$route && next.$$route.layout) {
       var layoutUrl = next.$$route.layout;
-      
-      // Layout dosyasını yükle
-      $templateRequest(layoutUrl).then(function(template) {
-        var layoutElement = angular.element(document.documentElement); // Tüm HTML elementini al
-        layoutElement.html(template); // Yeni layout'u DOM'a ekle
-        $compile(layoutElement.contents())($rootScope); // AngularJS template'ini derle
+
+      // Layout-container'ı kontrol edin ve içeriği temizleyin
+      var layoutElement = angular.element(document.querySelector('#layout-container'));
+      layoutElement.empty(); // Mevcut layout'u temizleyin
+
+      // Yeni layout'u yükleyin
+      $templateRequest(layoutUrl).then(function (template) {
+        layoutElement.html(template); // Yeni layout'u ekleyin
+        $compile(layoutElement.contents())($rootScope); // AngularJS template'ini derleyin
       });
     }
 
@@ -162,5 +156,4 @@ app.run(function($rootScope, $location, authService, $translate, $templateReques
     $rootScope.selectedLanguage = $translate.preferredLanguage(); // Varsayılan dil
   }
 });
-
 
